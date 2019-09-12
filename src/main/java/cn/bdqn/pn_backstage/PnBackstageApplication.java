b53c7b4fa5079af3ffc9e9585d2ba4f05857ed65
13546721611 <19991008hyd>
2019-09-12 11:03:08 +0800

    20190912。11

--- src/main/java/cn/bdqn/pn_backstage/PnBackstageApplication.java
+++ src/main/java/cn/bdqn/pn_backstage/PnBackstageApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PnBackstageApplication {
 
-	public static void main(String[] args) {
+	public statics void main(String[] args) {
 		SpringApplication.run(PnBackstageApplication.class, args);
 	}
 
--- src/main/java/cn/bdqn/pn_backstage/mapper/AnswerMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.bdqn.pn_backstage.mapper;
-
-
-import cn.bdqn.demo.pojo.Questions;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerMapper extends JpaRepository<Questions,Integer>{
-
-}
